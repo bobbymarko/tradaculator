9565ea83ff2b98698fa8ba1ae4a505fdde3e16cc
Bobby Marko <bobby@bobby-marko.com>
2011-08-22 20:55:18 -0500

    starting to pull in data from remix

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,25 @@
-if (navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i)) {
-var viewportmeta = document.querySelectorAll('meta[name="viewport"]')[0];
-if (viewportmeta) {
-viewportmeta.content = 'width=device-width, minimum-scale=1.0, maximum-scale=1.0';
-document.body.addEventListener('gesturestart', function() {
-viewportmeta.content = 'width=device-width, minimum-scale=0.25, maximum-scale=1.6';
-}, false);
-}
-}
+$(function(){
+	$.ajax({
+	  url: "http://api.remix.bestbuy.com/v1/products?page=1&sort=salePrice&format=json&apiKey=amfnpjxnz6c9wzfu4h663z6w",
+	  dataType: "jsonp",
+	  cache:true,
+	  success: function(data){
+	    console.log(data);
+	    var html = '';
+	    $.each(data.products, function(k,v){
+   		    html += '<article class="hproduct">';
+   		    html += '<div id="price_block">'+v.salePrice+'<a href="'+v.addToCartUrl+'">Add To Cart</a></div>';
+			html += '<figure>';
+			html += 	'<img src="'+v.image+'" alt="" />';
+			html += '</figure>';
+			html += '<header>';
+			html += 	'<h1>'+v.name+'</h1>';
+			html += '</header>';
+			
+			html += '</article>';
+	    });
+	    $('#product_list').append(html);
+
+	  }
+	});
+});
