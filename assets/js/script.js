fe6a74771cbb05aee2e90f1ab2f86129d0ead27f
Bobby Marko <bobby@bobby-marko.com>
2011-08-26 07:49:13 -0500

    beginning to match design's styles

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,24 @@
 $(function(){
 	$.ajax({
-	  url: "http://api.remix.bestbuy.com/v1/products?page=1&format=json&apiKey=amfnpjxnz6c9wzfu4h663z6w",
+	  url: "http://api.remix.bestbuy.com/v1/products(search=mario&type=game)?format=json&apiKey=amfnpjxnz6c9wzfu4h663z6w",
 	  dataType: "jsonp",
 	  cache:true,
 	  success: function(data){
 	    console.log(data);
 	    var html = '';
 	    $.each(data.products, function(k,v){
-   		    html += '<article class="hproduct">';
-   		    html += '<div id="price_block">'+v.salePrice+'<a href="'+v.addToCartUrl+'">Add To Cart</a></div>';
-			html += '<figure>';
-			html += 	'<img src="'+v.image+'" alt="" />';
-			html += '</figure>';
-			html += '<header>';
-			html += 	'<h1>'+v.name+'</h1>';
-			html += '</header>';
-			
-			html += '</article>';
+	    	if (v.tradeInValue){
+	   		    html += '<article class="hproduct">';
+				html += '<figure><div>';
+				html += 	'<img src="'+v.image+'" alt="" />';
+				html += '</div></figure>';
+				html += '<header>';
+				html += 	'<h1>'+v.name.split(' - ')[0]+'</h1>';
+				html += 	'<p>'+v.name.split(' - ')[1]+'</p>';
+				html += '</header>';
+				html += '<div id="price_block">$'+v.tradeInValue+'.00</div>';
+				html += '</article>';
+			}
 	    });
 	    $('#product_list').append(html);
 
