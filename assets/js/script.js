56c1d097a979d7e3d550065c46df4f01b66b45c5
Bobby Marko <bobby@bobby-marko.com>
2011-08-27 17:09:28 -0500

    Continuing to style stuff

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,8 @@
 $(function(){
 	$.ajax({
-	  url: "http://api.remix.bestbuy.com/v1/products(search=mario&type=game)?format=json&apiKey=amfnpjxnz6c9wzfu4h663z6w",
-	  dataType: "jsonp",
+	  url: 'assets/js/sample_response.js',
+	  //url: "http://api.remix.bestbuy.com/v1/products(search=call&type=game)?page=2&format=json&apiKey=amfnpjxnz6c9wzfu4h663z6w",
+	  dataType: "json",
 	  cache:true,
 	  success: function(data){
 	    console.log(data);
@@ -16,7 +17,7 @@ $(function(){
 				html += 	'<h1>'+v.name.split(' - ')[0]+'</h1>';
 				html += 	'<p>'+v.name.split(' - ')[1]+'</p>';
 				html += '</header>';
-				html += '<div id="price_block">$'+v.tradeInValue+'.00</div>';
+				html += '<div class="price_block_wrapper"><div class="price_block"><span class="vendor_name">Best Buy</span> <span class="trade_in_value">$'+v.tradeInValue+'.00<span></div></div>';
 				html += '</article>';
 			}
 	    });
