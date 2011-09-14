f9f13f8fd4507dcd51403eaa39e3f7fff72fa04d
Bobby Marko <bobby@bobby-marko.com>
2011-09-13 23:00:16 -0500

    attempting to not use any server side code for project...

--- public/j.js
+++ public/j.js
@@ -61,8 +61,50 @@ function get_games(query,page){
 	$('h3').remove();
 	$('#pl').after('<h3 id="l">&#x203B;</h3>');
 	loading = true;
+	
 	$.ajax({
-	  //url: 'http://0.0.0.0:9292/values/'+query+'/'+page,
+		url:'http://api.remix.bestbuy.com/v1/products(tradeInValue%3E0&active=*&type=game)?format=json&page='+page+'&show=tradeInValue,image,name,upc&apiKey=amfnpjxnz6c9wzfu4h663z6w',
+		dataType:'jsonp',
+		cache:true,
+		success:function(data) {
+			$('#l').remove()
+			html = '';
+			if (data.products){
+				$.each(data.products, function(k,v){
+					if (v.tradeInValue){
+			    		var n = v.name.split(' - ');
+			   		    html += '<div class="p">';
+			   		    html += '<div class="pw">';
+						html += '<div class="f">';
+						html += 	'<img src="'+v.image+'" alt="" />';
+						html += '</div>';
+						html += '<header>';
+						html += 	'<h1>'+n[0]+'</h1>';
+						html += 	'<p>'+n[n.length - 1]+'</p>';
+						html += '</header>';
+						html += '<div class="pbw"><div class="b"><a class="vn" target="_blank" href="'+sortable[0].url+'">Trade It In</a><a href="#" class="tv">'+sortable[0].value+'</a></div>';
+						html += '<ul class="dd">';
+						$.each(sortable, function(k,v){
+							html += '<li><a target="_blank" href="'+v.url+'">'+v.vendor.replace('_',' ') +' <span>'+v.value+'</span></a></li>';
+						});
+						html += '</ul>';
+						html += '</div></div>';
+						html += '</div>';
+					}
+				});
+			}
+			if (html !== ''){
+			    $('#pl').append(html);
+			    loading = false;
+		    }else{
+		    	$('#pl').after("<h3>That's all</h3>");
+		    }
+		}
+	});
+	
+	
+	/*$.ajax({
+	  url: 'http://0.0.0.0:9292/values/'+query+'/'+page,
 	  url: 'http://beta.tradaculator.com/values/'+query+'/'+page,
 	  dataType: "jsonp",
 	  cache:true,
@@ -113,5 +155,5 @@ function get_games(query,page){
 	    }
 
 	  }
-	});
+	});*/
 }
