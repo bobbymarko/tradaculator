9bb0624150960508919803265ea880b3ad4bb994
bobbymarko <bobby@bobby-marko.com>
2011-10-19 20:24:26 -0500

    updating touch icons

--- app/controllers/trade_in_values_controller.rb
+++ app/controllers/trade_in_values_controller.rb
@@ -1,7 +1,7 @@
 class TradeInValuesController < ApplicationController
   respond_to :html, :json, :js
   caches_page :index # expired with a cron job on server
-  caches_action :index, :show, :cache_path => Proc.new { |c|
+  caches_action :show, :cache_path => Proc.new { |c|
     "#{Rails.env} #{c.params} #{c.request.xml_http_request?}"
   }
   
