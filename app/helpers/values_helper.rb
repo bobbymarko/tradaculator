766a728b3aae414096451ba2f96a6ebf27188680
bobbymarko <bobby@bobby-marko.com>
2011-10-26 07:40:06 -0500

    currency conversion was dropping cents. fixed.

--- app/helpers/values_helper.rb
+++ app/helpers/values_helper.rb
@@ -1,12 +1,12 @@
 module ValuesHelper
   def currency(number, opts = {})
     return if number.to_s.empty?
-  
+    number = number.to_f/100
     unit      = opts[:unit]      || '$'
     precision = opts[:precision] || 2
     separator = opts[:separator] || ', '
-  
-    ret = "%s%.#{Integer(precision)}f" % [unit,number/100]
+
+    ret = "%s%.#{Integer(precision)}f" % [unit,number]
     parts = ret.split('.')
     parts[0].gsub!(/(\d)(?=(\d\d\d)+(?!\d))/, "\\1#{separator}")
     parts.join('.')
