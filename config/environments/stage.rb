bb1042ad0a554177051e0e256e7e7b046a91acee
bobbymarko <bobby@bobby-marko.com>
2011-10-26 23:59:52 -0500

    attempting to resolve precompilition issues

--- config/environments/stage.rb
+++ config/environments/stage.rb
@@ -43,7 +43,8 @@ Tradaculator::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( jquery.js, excanvas.js )
+  config.assets.precompile += %w( jquery.js )
+  config.assets.precompile += %w( excanvas.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
