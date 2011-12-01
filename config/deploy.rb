55f14065ae5b5bc0952b84eb6790dd19fd5aba5c
bobbymarko <bobby@bobby-marko.com>
2011-11-30 19:07:19 -0600

    switching to deploy from currently checked out branch

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,7 +17,7 @@ default_run_options[:pty] = true  # Must be set for the password prompt from git
 set :scm, "git"
 set :repository, "git@github.com:bobbymarko/tradaculator.git"  # Your clone URL
 set :deploy_via, :remote_cache
-set :branch, 'master'
+set :branch, $1 if `git branch` =~ /\* (\S+)\s/m
 
 set :user, "root"  # The server's user for deploys
 
