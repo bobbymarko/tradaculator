a3b04f1210b4052220633c45c9004b092edc6fdf
bobbymarko <bobby@bobby-marko.com>
2012-03-05 22:04:20 -0600

    gotta precompile html5shiv :(

--- config/environments/stage.rb
+++ config/environments/stage.rb
@@ -45,6 +45,7 @@ Tradaculator::Application.configure do
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   config.assets.precompile += %w( jquery.js )
   config.assets.precompile += %w( excanvas.js )
+  config.assets.precompile += %w( html5shiv.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
